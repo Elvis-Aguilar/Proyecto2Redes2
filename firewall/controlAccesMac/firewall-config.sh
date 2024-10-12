855e4f9d7fb5fdb3d1d88d2663de52de582b9902
Elvis Aguilar <elvisaguilar201930304@cunoc.edu.gt>
2024-10-12 16:29:31 -0600

    correccion sintaxis

--- firewall/controlAccesMac/firewall-config.sh
+++ firewall/controlAccesMac/firewall-config.sh
@@ -2,10 +2,9 @@
 
 # Definir las variables
 TABLE="Firewall"
-INTERFACE_ETH="enp8s0"  # La interfaz conectada la intranet
-INTERFACE_WIFI="wlp9s0"  # La interfaz Wi-Fi conectada al proxy
-ADMIN_MAC="00:e0:4c:36:00:89"  # MAC  máquina administradora
-# Archivo que contiene las MACs permitidas
+INTERFACE_ETH="enp8s0"  # La interfaz Ethernet conectada a la máquina administradora
+INTERFACE_WIFI="wlp9s0"  # La interfaz Wi-Fi conectada al router
+ADMIN_MAC="00:e0:4c:36:00:89" # Reemplaza con la MAC de tu máquina administradora
 FILE="acceso.mac"
 
 # Limpiar las reglas anteriores
@@ -14,14 +13,14 @@ nft flush ruleset
 # Crear la tabla de firewall
 nft add table ip $TABLE
 
-# Crear un set para las MACs
-nft add set ip $TABLE macs { type ether_addr; flags interval; }
+# Crear un set para las MACs permitidas
+nft add set ip $TABLE macs { type ether_addr\; }
 
 # Crear una cadena para el tráfico de entrada (INPUT)
-nft add chain ip $TABLE input { type filter hook input priority 0; }
+nft add chain ip $TABLE input { type filter hook input priority 0\; policy drop\; }
 
 # Crear una cadena para el reenvío (FORWARD)
-nft add chain ip $TABLE forward { type filter hook forward priority 0; }
+nft add chain ip $TABLE forward { type filter hook forward priority 0\; policy drop\; }
 
 # Permitir siempre SSH desde la MAC de la máquina administradora
 nft add rule ip $TABLE input iif $INTERFACE_ETH ether saddr $ADMIN_MAC tcp dport 22 accept
@@ -41,15 +40,12 @@ fi
 # Permitir tráfico ICMP (ping) solo para las MACs permitidas
 nft add rule ip $TABLE input iif $INTERFACE_ETH ether saddr @macs ip protocol icmp accept
 
-# Bloquear el tráfico ICMP para todas las demás MACs
-nft add rule ip $TABLE input iif $INTERFACE_ETH ether saddr != @macs drop
-
 # Configuración de NAT (Enmascaramiento)
 nft add table ip nat
-nft add chain ip nat postrouting { type nat hook postrouting priority 100; }
+nft add chain ip nat postrouting { type nat hook postrouting priority 100\; }
 nft add rule ip nat postrouting oif "$INTERFACE_WIFI" masquerade
 
-# Regla para bloquear todo lo que no esté en la lista de MACs permitidas
+# Regla para bloquear todo lo que no esté en la lista de MACs permitidas (input)
 nft add rule ip $TABLE input iif $INTERFACE_ETH ether saddr != @macs drop
 
 echo "Reglas del firewall configuradas con éxito."
